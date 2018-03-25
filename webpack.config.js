4dc7cd7c2b0b535e668604701d9fcf8a6264e353
ilynne <ilynne@gmail.com>
2018-03-24 21:41:24 -0700

    adds webpack and yarn

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // webpack folder’s entry js — excluded from jekll’s build process.
+  entry: "./webpack/entry.js",
+  output: {
+    // we’re going to put the generated file in the assets folder so jekyll will grab it.
+    // if using GitHub Pages, use the following:
+    // path: "assets/javascripts"
+    path: '/Users/lynne/Documents/MyBus/assets/javascripts',
+    filename: 'bundle.js'
+  },
+};
